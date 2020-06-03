48a4fdd69e0f9226a701fcd3864f8894f5f17c7e
Sebastiaan Jansen <whitebird@live.be>
2020-06-03 22:28:33 +0200

    added comments

--- source.js
+++ source.js
@@ -13,14 +13,34 @@ function send() {
 
 }
 
-function handle(e) {
+function handleKeypress(e) {
     if (e.keyCode === 13) {
         e.preventDefault();
         send();
     }
 }
 
+
 async function connect() {
+    let reader;
+    async function readLoop() {
+        console.log('Readloop');
+
+        while (true) {
+            const { value, done } = await reader.read();
+
+            if (value) {
+                log.textContent += value;
+                log.scrollTop = log.scrollHeight;
+            }
+            if (done) {
+                console.log('[readLoop] DONE', done);
+                reader.releaseLock();
+                break;
+            }
+        }
+    }
+
     const inputField = document.getElementById("input");
     inputField.disabled = false;
     inputField.focus();
@@ -51,23 +71,6 @@ async function writeToStream(line) {
     writer.releaseLock();
 }
 
-async function readLoop() {
-    console.log('Readloop');
-
-    while (true) {
-        const { value, done } = await reader.read();
-
-        if (value) {
-            log.textContent += value;
-            log.scrollTop = log.scrollHeight;
-        }
-        if (done) {
-            console.log('[readLoop] DONE', done);
-            reader.releaseLock();
-            break;
-        }
-    }
-}
 
 const BUFFER_SIZE = 32
 
@@ -76,7 +79,7 @@ function testFile() {
         `
 import time
 for i in range(0,100):
-    print("Hello " + str(i))
+    print("Hello3 " + str(i))
     time.sleep(1)
 `
     put(data)
@@ -87,57 +90,30 @@ function timeout(ms) {
 }
 
 async function enterRawRepl() {
+    // http://docs.micropython.org/en/latest/reference/repl.html#raw-mode
+
     console.log('entering raw REPL');
 
+    // sequence comes from ampy 
     // ctrl-C twice: interrupt any running program
-    // self.serial.write(b'\r\x03')
-    // time.sleep(0.1)
-    // self.serial.write(b'\x03')
-    // time.sleep(0.1)
-    await writeToStream('\r\x03')
+    await writeToStream('\r\x03');
     await timeout(100);
-    await writeToStream('\x03')
+    await writeToStream('\x03');
     await timeout(100);
 
-    // flush input (without relying on serial.flushInput())
-    // n = self.serial.inWaiting()
-    // while n > 0:
-    //     self.serial.read(n)
-    //     n = self.serial.inWaiting()
+    // ctrl-A: enter raw REPL
+    await writeToStream('\r\x01');
 
-    // self.serial.write(b'\r\x01') # ctrl-A: enter raw REPL
-    // data = self.read_until(1, b'raw REPL; CTRL-B to exit\r\n>')
-    // if not data.endswith(b'raw REPL; CTRL-B to exit\r\n>'):
-    //     print(data)
-    //     raise PyboardError('could not enter raw repl')
-    await writeToStream('\r\x01')
-
-    // self.serial.write(b'\x04') # ctrl-D: soft reset
-    // data = self.read_until(1, b'soft reboot\r\n')
-    // if not data.endswith(b'soft reboot\r\n'):
-    //     print(data)
-    //     raise PyboardError('could not enter raw repl')
     await timeout(100);
-    await writeToStream('\x04')
+    // ctrl-D: soft reset
+    await softReset();
 
-    // # By splitting this into 2 reads, it allows boot.py to print stuff,
-    // # which will show up after the soft reboot and before the raw REPL.
-    // # Modification from original pyboard.py below:
-    // #   Add a small delay and send Ctrl-C twice after soft reboot to ensure
-    // #   any main program loop in main.py is interrupted.
-    // time.sleep(0.5)
-    // self.serial.write(b'\x03')
-    // time.sleep(0.1)           # (slight delay before second interrupt
-    // self.serial.write(b'\x03')
-    // # End modification above.
-    // data = self.read_until(1, b'raw REPL; CTRL-B to exit\r\n')
-    // if not data.endswith(b'raw REPL; CTRL-B to exit\r\n'):
-    //     print(data)
-    //     raise PyboardError('could not enter raw repl')
+    // Add a small delay and send Ctrl-C twice after soft reboot to ensure
+    // any main program loop in main.py is interrupted.
     await timeout(500);
-    await writeToStream('\x03')
+    await writeToStream('\x03');
     await timeout(100);
-    await writeToStream('\x03')
+    await writeToStream('\x03');
 }
 
 async function exitRawRepl() {
@@ -145,43 +121,46 @@ async function exitRawRepl() {
     await writeToStream('\r\x02');
 }
 
-async function put(cmds) {
+/*
+Put stores the argument cmds to the flash on board as the file named filename (main.py default) via rawrepl.
+*/
+async function put(cmds, filename = 'main.py', autoExec = true) {
     await enterRawRepl();
-    //cmds = "a = 42;print(a)"
-
     console.log("uploading and executing", cmds);
-    const size = cmds.length;
 
-    // # Loop through and write a buffer size chunk of data at a time.
-    // for i in range(0, size, BUFFER_SIZE):
-    await writeToStream("f = open('main.py', 'wb')\r");
+    // Loop through each line and write a buffer size chunk of data at a time.
+    await writeToStream("f = open('" + filename + "', 'wb')\r");
 
     const array = cmds.split('\n')
     for (let i = 0; i < array.length; i++) {
+        const line = array[i];
 
-        const cmd = array[i];
-        await writeToStream("f.write('" + cmd + "\\n')\r");
-        console.log('cmd', cmd);
+        for (let j = 0; j < array[i].length; j += BUFFER_SIZE) {
+            const chunk = line.slice(j, Math.min(array[i].length, j + BUFFER_SIZE))
+            await writeToStream("f.write('" + chunk + "')\r");
+        }
+
+        await writeToStream("f.write('\\n')\r");
     }
-
-    // for (let i = 0; i < size; i += BUFFER_SIZE) {
-    //     // chunk_size = min(BUFFER_SIZE, size - i)
-    //     chunk_size = Math.min(BUFFER_SIZE, size - i)
-    //     // chunk = repr(data[i : i + chunk_size])
-    //     const chunk = cmds.substring(i, i + chunk_size);
-    //     console.log('chunk', chunk);
-
-    //     //     if not chunk.startswith("b"):
-    //     //         chunk = "b" + chunk
-    //     //     self._pyboard.exec_("f.write({0})".format(chunk))
-    //     await writeToStream("f.write('" + chunk + "')\r");
-    // }
     await writeToStream("f.close()\r");
     await timeout(100);
-    await writeToStream('\x04')
+
+    // send CTRL signal to execute instructions in raw repl
+    await softReset();
     await timeout(100);
+
     await exitRawRepl();
+
+    // reset if needed
+    autoExec && await softReset();
+
 }
 
+async function softReset() {
+    await writeToStream('\x04');
+
+}
+
+// to quickly check the file via the python repl
 // f = open("main.py", "r")
 // print(f.readlines())
